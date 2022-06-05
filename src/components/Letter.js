9f3abb72606513a441fb6a9135bc51e5c0af7c42
netanyaonthenet <nmirissat@outlook.com>
2022-06-04 22:55:54 -0400

    add project

---
+++ src/components/Letter.js
@@ -0,0 +1,22 @@
+import React, { useContext, useEffect } from "react";
+import { AppContext } from "../App";
+
+function Letter({letterPos, attemptVal}) {
+    //what letter represents it's cell
+    const { board, correctWord, currAttempt, setDisabledLetters } = useContext(AppContext);
+    const letter = board[attemptVal][letterPos];
+    const correct = correctWord.toUpperCase()[letterPos] === letter;
+    console.log(correctWord);
+    const almost = !correct && letter !== "" && correctWord.toUpperCase().includes(letter);
+    const letterState = currAttempt.attempt > attemptVal && (correct ? "correct" : almost ? "almost" : "error");
+
+    useEffect(() => {
+      if(letter !== "" && !correct && !almost){
+        setDisabledLetters((prev) => [...prev, letter]);
+      }
+    }, [currAttempt.attempt]);
+  return ( <div className="letter" id={letterState}>{letter}</div> );
+
+}
+
+export default Letter
